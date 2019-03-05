856a03ea7bbf41453647387476412140c4fd8466
zhengtuqiang <18218791448@139.com>
2019-03-06 00:56:47 +0800

    自定义Observable

--- app/src/main/java/com/nanchen/rxjava2examples/module/HomeActivity.java
+++ app/src/main/java/com/nanchen/rxjava2examples/module/HomeActivity.java
@@ -7,6 +7,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
@@ -16,11 +17,18 @@ import com.nanchen.rxjava2examples.base.BaseViewPagerAdapter;
 import com.nanchen.rxjava2examples.constant.GlobalConfig;
 import com.nanchen.rxjava2examples.module.rxjava2.operators.OperatorsFragment;
 import com.nanchen.rxjava2examples.module.rxjava2.usecases.UseCasesFragment;
-import com.nanchen.rxjava2examples.module.web.WebViewActivity;
 import com.nanchen.rxjava2examples.util.ScreenUtil;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.OnClick;
+import io.reactivex.Observable;
+import io.reactivex.ObservableSource;
+import io.reactivex.Observer;
+import io.reactivex.functions.Consumer;
+import io.reactivex.functions.Function;
 
 public class HomeActivity extends BaseActivity {
 
@@ -78,7 +86,41 @@ public class HomeActivity extends BaseActivity {
 
     @OnClick(R.id.fab)
     public void onViewClicked() {
-        WebViewActivity.start(this,"https://github.com/nanchen2251","我的GitHub,欢迎Star");
+        // WebViewActivity.start(this,"https://github.com/nanchen2251","我的GitHub,欢迎Star");
+        List<String> names = new ArrayList<>();
+        names.add("zhangsan");
+        names.add("lisi");
+        names.add("wangwu");
+        my(names).flatMap(new Function<String, ObservableSource<String>>() {
+            @Override
+            public ObservableSource<String> apply(String s) throws Exception {
+                return Observable.just(s + "1", s + "2", s + "3");
+            }
+        }).subscribe(new Consumer<String>() {
+            @Override
+            public void accept(String s) throws Exception {
+                Log.d("observable.my()", "accept: " + s);
+            }
+        });
+    }
+
+    private <T> Observable<T> my(List<T> t) {
+        return new ObservableMy<>(t);
+    }
+
+    static class ObservableMy<T> extends Observable<T> {
+        private List<T> mT;
+
+        public ObservableMy(List<T> t) {
+            mT = t;
+        }
+
+        @Override
+        protected void subscribeActual(Observer<? super T> observer) {
+            for (T t : mT) {
+                observer.onNext(t);
+            }
+        }
     }
 
     @Override
